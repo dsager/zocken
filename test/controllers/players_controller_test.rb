4681c79ce34a73597860bdbfabefa70a1f1480f5
Daniel Sager <daniel@sager1.de>
2015-05-24 17:56:18 +0200

    updated test

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -11,19 +11,6 @@ class PlayersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:players)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create player" do
-    assert_difference('Player.count') do
-      post :create, player: { email: @player.email, name: @player.name, password: 'secret', password_confirmation: 'secret', points: @player.points }
-    end
-
-    assert_redirected_to player_path(assigns(:player))
-  end
-
   test "should show player" do
     get :show, id: @player
     assert_response :success
@@ -34,10 +21,17 @@ class PlayersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should update player" do
-    patch :update, id: @player, player: { email: @player.email, name: @player.name, password: 'secret', password_confirmation: 'secret', points: @player.points }
-    assert_redirected_to player_path(assigns(:player))
-  end
+  # test "should update player" do
+  #   patch :update, id: @player,
+  #         player: {
+  #           email: @player.email,
+  #           name: @player.name,
+  #           password: 'secret',
+  #           password_confirmation: 'secret',
+  #           points: @player.points }
+  #   assert_redirected_to player_path(assigns(:player))
+  # end
+  # FIXME: change devise route
 
   test "should destroy player" do
     assert_difference('Player.count', -1) do
