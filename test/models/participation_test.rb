bc418acaa003949591eacbed7916dc03ff85fb2e
Daniel Sager <daniel@sager1.de>
2015-05-27 00:41:00 +0200

    fixed bet closed message test

--- test/models/participation_test.rb
+++ test/models/participation_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 class ParticipationTest < ActiveSupport::TestCase
   def setup
+    @participation = participations(:klaus_funny_bet)
+    @result = @participation.result
     @player = MiniTest::Mock.new
-    @participation = Participation.allocate
   end
 
   test '#bet_closed calls #bet_closed on player' do
-    @player.expect(:bet_closed, true, [true, :bet])
+    @player.expect(:bet_closed, true, [@result])
     @participation.stub(:bet, :bet) do
       @participation.stub(:player, @player) do
-        assert @participation.bet_closed(true)
+        assert @participation.bet_closed
       end
     end
   end
